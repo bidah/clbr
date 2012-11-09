7f2aae5fe844a9562f68ec9c50f68cd49de5f726
Rodrigo Figueroa <barto@colaboractive.com>
2012-11-09 14:09:28 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AyoApp::Application.config.secret_token = '866a9651061ea9f43541742a933e3fd74ef5908e89a89d070da92222bd975c78d65ef103158a9f8df8e08194e10c91795415c22dd3b41c7b32a06809261d955e'
